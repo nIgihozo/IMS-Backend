8e461f5d1f0afa237f6178fd437787bd9966f7c4
nIgihozo <n.igihozo@alustudent.com>
2026-07-16 20:40:46 +0200

    Setting tables for database

--- ims_backend/settings.py
+++ ims_backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,8 +42,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'users',
 ]
 
+AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
---
+++ users/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        STUDENT = 'STUDENT', 'Student'
+        SUPERVISOR = 'SUPERVISOR', 'School Supervisor'
+        COMPANY = 'COMPANY', 'Company'
+        ADMIN = 'ADMIN', 'Admin'
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=30, choices=Roles.choices, default=Roles.STUDENT)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+            return f"{self.email} ({self.role})"
+        
+class StudentProfile(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
+        full_name = models.CharField(max_length=255)
+        tvetstudent_id = models.CharField(max_length=100, unique=True)
+        course_area = models.CharField(max_length=100)
+        school_name = models.CharField(max_length=255)
+
+        def __str__(self):
+            return f"{self.full_name} ({self.tvetstudent_id})"
+        
+class CompanyProfile(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='company_profile')
+        company_representative_name = models.CharField(max_length=255)
+        company_name = models.CharField(max_length=255)
+        company_sector = models.CharField(max_length=100)
+        rdb_registration_number = models.CharField(max_length=100, unique=True)
+        company_name = models.CharField(max_length=255)
+        company_address = models.TextField()
+
+        def __str__(self):
+            return f"{self.company_representative_name} ({self.company_name})"
+        
+class SupervisorProfile(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='supervisor_profile')
+        full_name = models.CharField(max_length=255)
+        school_name = models.CharField(max_length=255)
+        department = models.CharField(max_length=100)
+
+        def __str__(self):
+            return f"{self.full_name} ({self.school_name})"
